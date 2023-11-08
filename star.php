8dae2c785d7c09af8dd256bc49de3c3469c57f8d
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-11-08 01:48:44 -0600

    Update star.php

--- star.php
+++ star.php
@@ -1,4 +1,4 @@
-<?php  if (isset($country) && $country == "ES" || strpos("Europe", $timezone)) { $proxy = "https://slowdus.herokuapp.com/"; } else { $proxy = ""; } ?>
+<?php  if (isset($country) && $country == "ES" || strpos("Europe", $timezone)) { $proxy = "https://slowdus.herokuapp.com/"; echo "Estás usando proxy en" . $country; } else { $proxy = ""; echo "No estás usando proxy en " .$country; } ?>
 <div class="section mt-2">
     <h2>Star+ <small>Eventos Programados</small></h2>
     <div id="eventos" class="row row-cols-2 row-cols-sm-2 row-cols-lg-3 gx-3 gx-md-4 mt-n2 mt-sm-0">
