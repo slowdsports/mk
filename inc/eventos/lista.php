61dcb2d9d21f3a4c6feeba863c6910280dc3ee10
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-02-11 11:53:43 -0600

    api img

--- inc/eventos/lista.php
+++ inc/eventos/lista.php
@@ -29,7 +29,7 @@
                         <div class="card product-card liga-card">
                             <div class="card-body">
                                 <center>
-                                    <img src="https://api.sofascore.app/api/v1/unique-tournament/<?= $liga_id ?>/image" class="image liga-img" alt="<?= $result['ligaNombre'] ?>" />
+                                    <img src="https://api.codetabs.com/v1/proxy/?quest=https://api.sofascore.app/api/v1/unique-tournament/<?= $liga_id ?>/image" class="image liga-img" alt="<?= $result['ligaNombre'] ?>" />
                                     <h2 class="title text-center"><?= $result['ligaNombre'] ?></h2>
                                 </center>
                             </div>
