cca9f0cef94f310db33d71f363547c8a31dace55
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-02-20 23:33:28 -0600

    Update vix.php

--- vix.php
+++ vix.php
@@ -1,206 +1,125 @@
-<?php
-if (isset($country) && $country == "ES" || strpos($timezone, "rope")) {
-    $proxy = "https://slowdus.herokuapp.com/";
-    $_SESSION['message'] = "Estás usando proxy en " . $timezone . ". Si crees que es un error, contáctanos mediante el chat";
-    $_SESSION['color'] = "28a745";
-} else {
-    $proxy = "";
-    $_SESSION['message'] = "No estás usando proxy en " . $timezone . ". Si crees que es un error, contáctanos mediante el chat";
-    $_SESSION['color'] = "28a745";
-}
-?>
+<div class="header-large-title">
+    <h1 class="title">
+        Vix +
+    </h1>
+    <h4 class="subtitle">
+        <small>Eventos Programados</small>
+    </h4>
+</div>
 <div class="section full mt-2 mb-2">
-    <h2>Vix+ <small>Eventos Programados</small></h2>
     <div class="wide-block pt-2 pb-2">
-        <div id="eventos" class="row">
-            <!-- <script src="inc/eventos/star.js"></script> -->
-            <script>
-                document.addEventListener("DOMContentLoaded", function () {
-                    // Eliminar container
-                    var basuraContain = document.getElementById('appCapsule');
-                    basuraContain.classList.remove("container");
-                    var jsonUrl = "https://corsproxy.io/?https://futbolplaytvhd.com/vix.json";
-                    var eventosContainer = document.getElementById("eventos");
+        <div class="row" id="eventos"></div>
+        <script>
+            function eventos() {
+                var x = Math.random().toString(36).substring(7);
 
-                    // Sortear
-                    function compararStatus(a, b) {
-                        var statusOrden = {
-                            "EN VIVO": 0,
-                            "FINALIZADO": 2
-                        };
-                        if (a.status in statusOrden && b.status in statusOrden) {
-                            return statusOrden[a.status] - statusOrden[b.status];
-                        } else if (a.status in statusOrden) {
-                            return -1;
-                        } else if (b.status in statusOrden) {
-                            return 1;
-                        } else if (a.status.match(/^\d{2}:\d{2}$/) && b.status.match(/^\d{2}:\d{2}$/)) {
-                            // Ambos están en formato HH:MM
-                            var [aHours, aMinutes] = a.status.split(':').map(Number);
-                            var [bHours, bMinutes] = b.status.split(':').map(Number);
-                            if (aHours !== bHours) {
-                                return aHours - bHours;
-                            } else {
-                                return aMinutes - bMinutes;
+                $.ajax({
+                    url: "https://corsproxy.io/?url=https://deportestvhd2.com/vix.json?" + x,
+                    //url: "datos.json?" + x,
+                    type: "get",
+                    success: function (arr) {
+                        // Ordenar los eventos según su status
+                        arr.sort(function (a, b) {
+                            if (a.status === "EN VIVO") return -1;
+                            if (b.status === "EN VIVO") return 1;
+
+                            // Ordenar por formato "hora:minuto" si es aplicable
+                            if (a.status.match(/^\d{1,2}:\d{2}$/) && b.status.match(/^\d{1,2}:\d{2}$/)) {
+                                return new Date("1970-01-01T" + a.status) - new Date("1970-01-01T" + b.status);
                             }
-                        } else {
+
+                            // Si no es EN VIVO ni formato "hora:minuto", ordenar por FINALIZADO
+                            if (a.status === "FINALIZADO") return 1;
+                            if (b.status === "FINALIZADO") return -1;
+
+                            // Otros casos
                             return 0;
-                        }
-                    }
+                        });
+                        var content = '';
 
-                    function procesarURL(url) {
-                        if (url.trim() === "#") {
-                            return url;
-                        }
+                        for (var i = 0; i < arr.length; i++) {
 
-                        var parteDespreciable = "/embed/eventos/?r=";
-                        var urlSinParteDespreciable = url.replace(parteDespreciable, "");
-                        var desencriptada = atob(urlSinParteDespreciable);
-                        // Intenta reemplazar el primer regex
-                        if (/https:\/\/\S*\/star_jwp\.html\?get=/.test(desencriptada)) {
-                            desencriptada = desencriptada.replace(/https:\/\/\S*\/star_jwp\.html\?get=/, "");
-                        } else if (/^https:\/\/cdn\.sfndeportes\.net\/star_wspp\?get=/.test(desencriptada)) {
-                            // Si el primer regex no coincide, intenta el segundo regex
-                            desencriptada = desencriptada.replace(/^https:\/\/cdn\.sfndeportes\.net\/star_wspp\?get=/, "");
-                        }
-                        //console.log(desencriptada)
+                            var obj = arr[i];
 
-                        return desencriptada;
-                    }
-
-                    function encriptarContenido(eventoUrl) {
-                        if (eventoUrl.trim() !== "#") {
-                            var partes = eventoUrl.split("&");
-                            var urlEncriptada = partes
-                                .map(function (part) {
-                                    if (part.startsWith("key=") || part.startsWith("key2=") || part.startsWith("img=")) {
-                                        var igualIndex = part.indexOf("=");
-                                        var clave = part.substring(0, igualIndex + 1);
-                                        var valor = part.substring(igualIndex + 1);
-
-                                        if (clave === "img=") {
-                                            return clave + valor;
-                                        } else {
-                                            return clave + btoa(valor);
-                                        }
-                                    }
-                                    return part;
-                                })
-                                .join("&");
-
-                            var partesURL = urlEncriptada.split("&");
-                            var proxy = "<?= $proxy ?>";
-                            var primeraParte = partesURL[0];
-                            var primeraParteEncriptada = btoa(primeraParte);
-                            urlEncriptada = urlEncriptada.replace(primeraParte, primeraParteEncriptada);
-                            console.log(urlEncriptada)
-                            return urlEncriptada;
-                        }
-                        return eventoUrl;
-                    }
-
-                    function crearTarjeta(evento) {
-                        var card = document.createElement("div");
-                        card.className = "col-6 col-sm-4 col-md-3 evento";
-                        var eventoUrl = procesarURL(evento.url);
-                        var urlEncriptada = encriptarContenido(eventoUrl);
-
-                        var contenidoExtra = "";
-                        var contenidoIcon = "";
-                        var contenidoFlash = "";
-
-                        if (evento.status === "EN VIVO") {
-                            contenidoExtra = 'En Vivo';
-                            contenidoIcon = "ellipse";
-                            contenidoFlash = "faa-flash animated";
-                            contenidoColor = "light live-text";
-                        } else if (evento.status === "FINALIZADO") {
-                            contenidoExtra = 'Finalizado';
-                            contenidoIcon = "time-outline";
-                            contenidoColor = "primary";
-                        } else {
-                            contenidoExtra = `${evento.status}`;
-                            contenidoIcon = "hourglass-outline";
-                            contenidoColor = "success";
-                        }
-
-                        var $remaining = "";
-
-                        if (evento.status.includes(":")) {
-                            var eventTime = new Date();
-                            var eventParts = evento.status.split(":");
-                            eventTime.setHours(parseInt(eventParts[0], 10));
-                            eventTime.setMinutes(parseInt(eventParts[1], 10));
-                            eventTime.setSeconds(0);
-
-                            function updateCountdown() {
-                                var now = new Date();
-                                var timeDiff = eventTime - now;
-                                var hours = Math.floor(timeDiff / (1000 * 60 * 60));
-                                var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-                                var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-                                card.innerHTML = `
-                                <a href="?p=tv&evento&ifr=${urlEncriptada}&title=${evento.title}" aria-label="${evento.title}">
-                                    <div class="card product-card">
-                                        <div class="card-body">
-                                            <img src="${evento.img}" class="image" alt="${evento.league}">
-                                            <h2 class="title">
-                                                ${evento.title}
-                                            </h2>
-                                            <div class="price">
-                                                <ion-icon class='${contenidoFlash}' name='${contenidoIcon}'></ion-icon>
-                                                ${hours}h ${minutes}m ${seconds}s
-                                            </div>
-                                            <a href="?p=tv&evento&ifr=${urlEncriptada}&title=${evento.title}" aria-label="${evento.league}" class="btn btn-sm btn-${contenidoColor} btn-block">
-                                                ${contenidoExtra}
-                                            </a>
-                                        </div>
-                                    </div>
-                                </a>`;
-                                setTimeout(updateCountdown, 1000);
+                            // Integrar lógicas aquí
+                            var url = obj['url'];
+                            if (url !== "#") {
+                                url = url.replace("/embed/eventos/?r=", "")
+                                var decodedUrl = atob(url);
                             }
+                            
 
-                            updateCountdown();
-                        } else {
-                            $remaining = `${evento.status}`;
-                        }
-
-                        card.innerHTML = `
-                        <a href="?p=tv&evento&ifr=${urlEncriptada}&title=${evento.title}" aria-label="${evento.league}">                        
+                            if (obj['status'] == "EN VIVO")
+                                content += `
+                        <div class="col-6 col-md-4 col-lg-3 evento">
+                            <a href="?p=tv&evento&ifr=${url}">
                             <div class="card product-card">
+                                <img src="${obj['img']}" alt="${obj['title']}" class="image">
                                 <div class="card-body">
-                                    <img src="${evento.img}" class="image" alt="${evento.league}">
-                                    <h2 class="title">
-                                        ${evento.title}
-                                    </h2>
-                                    <p class="text">
-                                        ${evento.league}
-                                    </p>
-                                    <a href="?p=tv&evento&ifr=${urlEncriptada}&title=${evento.title}" aria-label="${evento.league}" class="btn btn-sm btn-${contenidoColor} btn-block">
-                                        <ion-icon class='${contenidoFlash}' name='${contenidoIcon}'></ion-icon>
-                                        ${evento.status}
-                                    </a>
+                                <h2 class="title">${obj['title']}</h2>
+                                <p class="text">${obj['league']}</p>
+                                <a href="?p=tv&evento&ifr=${url}" class="btn btn-sm btn-light live-text btn-block">
+                                    <ion-icon class="faa-flash animated md hydrated" name="ellipse" role="img" aria-label="ellipse"></ion-icon>
+                                    ${obj['status']}
+                                </a>
                                 </div>
                             </div>
-                        </a>`;
+                            </a>
+                        </div>
+                        `;
 
-                        return card;
+                            else if (obj['status'] == "FINALIZADO")
+                                content += `
+                        <div class="col-6 col-md-4 col-lg-3 evento">
+                            <a>
+                            <div class="card product-card">
+                                <img src="${obj['img']}" alt="${obj['title']}" class="image">
+                                <div class="card-body">
+                                <h2 class="title">${obj['title']}</h2>
+                                <p class="text">${obj['league']}</p>
+                                <a href="?p=tv&r=${url}" class="disabled btn btn-sm btn-light live-text btn-block">
+                                    <ion-icon class="md hydrated" name="time-outline" role="img" aria-label="time-outline"></ion-icon>
+                                    ${obj['status']}
+                                </a>
+                                </div>
+                            </div>
+                            </a>
+                        </div>
+                        `;
+
+                            else
+                                content += `
+                        <div class="col-6 col-md-4 col-lg-3 evento">
+                            <a>
+                            <div class="card product-card">
+                                <img src="${obj['img']}" alt="${obj['title']}" class="image">
+                                <div class="card-body">
+                                <h2 class="title">${obj['title']}</h2>
+                                <p class="text">${obj['league']}</p>
+                                <a href="?p=tv&r=${url}" class="disabled btn btn-sm btn-light live-text btn-block">
+                                    <ion-icon class="md hydrated" name="calendar-outline" role="img" aria-label="calendar-outline"></ion-icon>
+                                    <span class="t">${obj['status']}</span> hs
+                                </a>
+                                </div>
+                            </div>
+                            </a>
+                        </div>
+                        `;
+                        }
+
+                        $("#eventos").html(content);
+
+                        guardaHorario();
+                        dT();
                     }
+                })
+            }
 
-                    fetch(jsonUrl)
-                        .then((response) => response.json())
-                        .then((data) => {
-                            data.sort(compararStatus);
-                            data.forEach((evento) => {
-                                var tarjeta = crearTarjeta(evento);
-                                eventosContainer.appendChild(tarjeta);
-                            });
-                        })
-                        .catch((error) => {
-                            console.error("Error al obtener el JSON:", error);
-                        });
-                });
-            </script>
-        </div>
+            eventos();
+
+            window.setInterval(function () {
+                eventos();
+            }, 60000);
+        </script>
     </div>
 </div>
